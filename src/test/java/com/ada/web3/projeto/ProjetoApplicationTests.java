2b55dd7145dd819ca121550cccfa2a97776aa6b4
Ramon Carvalho <rcarvalho.pb@gmail.com>
2023-04-17 12:42:13 -0300

    Projeto criado

---
+++ src/test/java/com/ada/web3/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.web3.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
